ea4dfbb21f4e56a4b9327c50699875bdf25159f7
NumeUtilizator <bogdan.andrei.roman@gmail.com>
2018-07-24 09:55:28 +0300

    gpio 23 only

--- gpio/gpio_example.py
+++ gpio/gpio_example.py
@@ -10,21 +10,21 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 # GPIO pins list based on GPIO.BOARD
-gpioList1 = [3,5,7,8,10,11,12,13,15]
-gpioList2 = [16,18,19,21,22,23,24,26]
-
+#gpioList1 = [3,5,7,8,10,11,12,13,15]
+#gpioList2 = [16,18,19,21,22,23,24,26]
+gpioList2 = [23]
 # Set mode for each gpio pin
-GPIO.setup(gpioList1, GPIO.OUT)
+#GPIO.setup(gpioList1, GPIO.OUT)
 GPIO.setup(gpioList2, GPIO.OUT)
 
 while True:
 	# Change gpio pins in list 1 from low to high and list 2 from high to low
-	GPIO.output(gpioList1, 1)
+	#GPIO.output(gpioList1, 1)
 	GPIO.output(gpioList2, 0)
 	time.sleep(1)
 
 	# Change gpio pin in list 1 from high to low and list 2 from low to high
-	GPIO.output(gpioList1, 0)
+	#GPIO.output(gpioList1, 0)
 	GPIO.output(gpioList2, 1)
 	time.sleep(1)
 
