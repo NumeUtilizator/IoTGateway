a89aec14c380019a34e6c42fce4182125fbac093
NumeUtilizator <bogdan.andrei.roman@gmail.com>
2018-08-05 15:36:24 +0300

    new feature

--- gpio/gpio_example.py
+++ gpio/gpio_example.py
@@ -13,18 +13,21 @@ GPIO.setmode(GPIO.BOARD)
 #gpioList1 = [3,5,7,8,10,11,12,13,15]
 #gpioList2 = [16,18,19,21,22,23,24,26]
 gpio2 = 18
+gpio1 = 11
 # Set mode for each gpio pin
-#GPIO.setup(gpioList1, GPIO.OUT)
+GPIO.setup(gpio1, GPIO.OUT)
 GPIO.setup(gpio2, GPIO.OUT)
 
 while True:
 	# Change gpio pins in list 1 from low to high and list 2 from high to low
 	#GPIO.output(gpioList1, 1)
+	GPIO.output(gpio1, 0)
 	GPIO.output(gpio2, 0)
 	time.sleep(1)
 
 	# Change gpio pin in list 1 from high to low and list 2 from low to high
 	#GPIO.output(gpioList1, 0)
+	GPIO.output(gpio1, 1)
 	GPIO.output(gpio2, 1)
 	time.sleep(1)
 
